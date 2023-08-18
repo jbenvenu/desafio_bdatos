1a1602b6d7d100e2efee5dc9721b0bfcdd2451e3
Paloma Belen Ramirez Castillo <palomabelenramirezcastillo@MacBook-Pro-de-Paloma.local>
2023-08-18 18:28:27 -0400

    Arreglo formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,89 +1,104 @@
 import React, {useState} from "react";
-import "./styles/Form.css"
 
-const Formulario =({setAlert}) => {
+const Formulario =({setAlert, onAgregarColaborador,contadorId}) => {
     const [nombre, setNombre] = useState ("");
     const [email, setEmail] = useState ("");
     const [edad, setEdad] = useState ("");
     const [cargo, setCargo] = useState ("");
     const [telefono, setTelefono] = useState ("");
-
-const validarDatos =(e) => {
+    const validarDatos = (e) => {
         e.preventDefault();
-
+    
         if (
-            nombre === "" ||
-            email   === "" ||
-            edad === "" ||
-            cargo === "" ||
-            telefono === ""
-        ) {
-            setAlert({
-                msg:"Completa los campos !",
-                color: "danger",
-            });
-            return;
+            nombre === '' || 
+            email === '' || 
+            edad === '' || 
+            cargo === '' || 
+            telefono === '') {
+          setAlert({
+            msg: 'Completa los campos!',
+            color: 'danger',
+          });
+          return;
         }
-
-        setAlert({
-         msg:"Colaborador agregado Correctamente !",
-         color : "success",
+    
+        onAgregarColaborador({
+          id: contadorId.toString(),
+          nombre,
+          email,
+          edad,
+          cargo,
+          telefono,
         });
-        setNombre("");
-        setEmail("");
-        setEdad("");
-        setCargo("");
-        setTelefono("");
-};
-
-return (
-    <>
-
-    <div className="customformx">
-        <form onSubmit={validarDatos}>
-            <input className="custominput"
-                type="text"
-                name="nombre"
-                placeholder="Nombre"
-                value={nombre}
-                onChange={(e) => setNombre (e.target.value)}
+    
+        setAlert({
+          msg: 'Colaborador agregado correctamente!',
+          color: 'success',
+        });
+    
+        setNombre('');
+        setEmail('');
+        setEdad('');
+        setCargo('');
+        setTelefono('');
+      };
+    
+      return (
+        <div className="custom-card">
+          <div className="card-body">
+            <h2 className="card-title">Agregar Colaborador</h2>
+            <form onSubmit={validarDatos}>
+              <div className="input-column">
+                <input
+                  className="custom-input"
+                  type="text"
+                  name="nombre"
+                  placeholder="Nombre"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
                 />
-            <input className="custominput"
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail (e.target.value)}
+                <input
+                  className="custom-input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
-            <input className="custominput"
-                type="number"
-                name="edad"
-                placeholder="Edad"
-                value={edad}
-                onChange={(e) => setEdad (e.target.value)}
+                <input
+                  className="custom-input"
+                  type="number"
+                  name="edad"
+                  placeholder="Edad"
+                  value={edad}
+                  onChange={(e) => setEdad(e.target.value)}
                 />
-            <input className="custominput"
-                type="text"
-                name="cargo"
-                placeholder="Cargo"
-                value={cargo}
-                onChange={(e) => setCargo (e.target.value)}
-                />    
-            <input className="custominput"
-                type="number"
-                name="telefono"
-                placeholder="Telefono"
-                value={telefono}
-                onChange={(e) => setTelefono (e.target.value)}
+                <input
+                  className="custom-input"
+                  type="text"
+                  name="cargo"
+                  placeholder="Cargo"
+                  value={cargo}
+                  onChange={(e) => setCargo(e.target.value)}
                 />
-                <div className="boxbutton">
-                    <button className="custombuttonx" type="submit">Agregar Colaborador</button>
-                </div>
-        </form>
+                <input
+                  className="custom-input"
+                  type="number"
+                  name="telefono"
+                  placeholder="Telefono"
+                  value={telefono}
+                  onChange={(e) => setTelefono(e.target.value)}
+                />
+              </div>
+              <div className="d-grid">
+                <button className="btn btn-primary" type="submit">
+                  Agregar Colaborador
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      );
+    };
 
-        </div>   
-    </>
-);   
-};
-
-export default Formulario;
+    export default Formulario;
