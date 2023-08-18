b1928ab2234b2f97423fdeeb0260de718ae41bec
Paloma Belen Ramirez Castillo <palomabelenramirezcastillo@MacBook-Pro-de-Paloma.local>
2023-08-18 18:26:42 -0400

    Buscados

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,16 +1,17 @@
+import React from 'react';
 
-
-function Buscador (){
-
-return (
-    <>
-    <input className="customsearch"
+function Buscador({ onBuscar }) {
+  return (
+    <div className="bg-primary text-white p-4">
+      <h2 className="mb-4">Buscar Colaborador</h2>
+      <input
         type="text"
-        name="nombre"
+        className="form-control"
         placeholder="Busca un Colaborador"
-    />
-     </>
-)
+        onChange={(e) => onBuscar(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default Buscador;
